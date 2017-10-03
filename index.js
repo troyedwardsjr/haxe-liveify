8a21466ffa475e544ad08f31811e8f34e2f1b7bb
Troy Edwards Jr <troyedwardsjr@gmail.com>
2017-10-03 14:42:02 -0400

    Clean up code and remove commander as dependency for now.

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ exports.__esModule = true;
 var fs = require("fs");
 var process = require("child_process");
 var chokidar = require("chokidar");
-var liveServer = require('live-server');
 var greenBG = "\x1b[42m";
 var redBG = "\x1b[41m";
 var resetBG = "\x1b[0m";
 var config = JSON.parse(fs.readFileSync('./hx-liveify.json', 'utf8'));
-var cp;
+var liveServer = require('live-server');
 var liveReload = (function () {
     var params = {
         port: 4000,
@@ -17,39 +16,42 @@ var liveReload = (function () {
     };
     liveServer.start(params);
 })();
-var liveify = chokidar.watch(config.src, { ignored: /(^|[\/\\])\../ }).on('all', function (event, path) {
-    if (event == 'change') {
-        console.log(greenBG + "Building..." + resetBG);
-        // Kill build processs if change is made.
-        if (cp != null) {
-            cp.kill('SIGINT');
-            cp.stdin.end();
-            cp.stdout.destroy();
-            cp.stderr.destroy();
+var liveify = (function () {
+    var cp;
+    chokidar.watch(config.src, { ignored: /(^|[\/\\])\../ }).on('all', function (event, path) {
+        if (event == 'change') {
+            console.log(greenBG + "Building..." + resetBG);
+            // Kill build processs if change is made.
+            if (cp != null) {
+                cp.kill('SIGINT');
+                cp.stdin.end();
+                cp.stdout.destroy();
+                cp.stderr.destroy();
+            }
+            // Run different build commands depending on compiler (haxe, openfl, lime).
+            if (config.compiler == "haxe") {
+                cp = process.spawn('haxe', [config.hxml]);
+            }
+            else {
+                cp = process.spawn('haxelib', ['run', config.compiler, 'build'].concat(config.platforms));
+            }
+            // Print stdout to console.
+            cp.stdout.on('data', function (data) {
+                if (typeof data === "string")
+                    console.log("" + greenBG + data);
+                else
+                    console.log("" + greenBG + data.toString());
+            });
+            cp.stderr.on('data', function (data) {
+                if (typeof data === "string")
+                    console.log("" + redBG + data);
+                else
+                    console.log("" + redBG + data.toString());
+            });
+            cp.on('exit', function (code) {
+                console.log(greenBG + "Process complete and exited with code: " + code + resetBG);
+            });
         }
-        // Run different build commands depending on compiler (haxe, openfl, lime).
-        if (config.compiler == "haxe") {
-            cp = process.spawn('haxe', [config.hxml]);
-        }
-        else {
-            cp = process.spawn('haxelib', ['run', config.compiler, 'build'].concat(config.platforms));
-        }
-        // Print stdout to console.
-        cp.stdout.on('data', function (data) {
-            if (typeof data === "string")
-                console.log("" + greenBG + data);
-            else
-                console.log("" + greenBG + data.toString());
-        });
-        cp.stderr.on('data', function (data) {
-            if (typeof data === "string")
-                console.log("" + redBG + data);
-            else
-                console.log("" + redBG + data.toString());
-        });
-        cp.on('exit', function (code) {
-            console.log(greenBG + "Process complete and exited with code: " + code + resetBG);
-        });
-    }
-});
+    });
+})();
 exports["default"] = liveify;
--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
 import * as fs from 'fs';
 import * as process from 'child_process';
 import * as chokidar from 'chokidar';
-const liveServer = require('live-server');
 
 interface Config {
   compiler: string;
@@ -14,54 +13,57 @@ interface Config {
 const greenBG : string = "\x1b[42m";
 const redBG : string = "\x1b[41m";
 const resetBG : string = "\x1b[0m";
-let config : Config = JSON.parse(fs.readFileSync('./hx-liveify.json', 'utf8'));
-let cp : process.ChildProcess;
+const config : Config = JSON.parse(fs.readFileSync('./hx-liveify.json', 'utf8'));
+const liveServer = require('live-server');
 
-const liveReload = (() => {
+const liveReload : void = (() => {
   const params = {
     port: 4000, 
     host: "0.0.0.0",
     root: config.out
   };
   liveServer.start(params);
-})()
+})();
 
-const liveify : chokidar.FSWatcher = chokidar.watch(config.src, {ignored: /(^|[\/\\])\../}).on('all', (event:string, path:string) => {
-  if(event == 'change') {
-    console.log(`${greenBG}Building...${resetBG}`);
+const liveify : void = (() => {
+  let cp : process.ChildProcess;
+  chokidar.watch(config.src, {ignored: /(^|[\/\\])\../}).on('all', (event:string, path:string) => {
+    if(event == 'change') {
+      console.log(`${greenBG}Building...${resetBG}`);
 
-    // Kill build processs if change is made.
-    if(cp != null) {
-      cp.kill('SIGINT');
-      cp.stdin.end();
-      cp.stdout.destroy();
-      cp.stderr.destroy();
+      // Kill build processs if change is made.
+      if(cp != null) {
+        cp.kill('SIGINT');
+        cp.stdin.end();
+        cp.stdout.destroy();
+        cp.stderr.destroy();
+      }
+
+      // Run different build commands depending on compiler (haxe, openfl, lime).
+      if(config.compiler == "haxe") {
+        cp = process.spawn('haxe', [config.hxml]);
+      } else {
+        cp = process.spawn('haxelib', ['run', config.compiler, 'build'].concat(config.platforms));
+      }
+
+      // Print stdout to console.
+      cp.stdout.on('data', (data: string | Buffer) => {
+        if(typeof data === "string") 
+          console.log(`${greenBG}${data}`);
+        else 
+          console.log(`${greenBG}${data.toString()}`);
+      });
+      cp.stderr.on('data', (data: string | Buffer) => {
+        if(typeof data === "string") 
+          console.log(`${redBG}${data}`);
+        else 
+          console.log(`${redBG}${data.toString()}`);
+      });
+      cp.on('exit', (code: number) => {
+        console.log(`${greenBG}Process complete and exited with code: ${code}${resetBG}`);
+      });
     }
-
-    // Run different build commands depending on compiler (haxe, openfl, lime).
-    if(config.compiler == "haxe") {
-      cp = process.spawn('haxe', [config.hxml]);
-    } else {
-      cp = process.spawn('haxelib', ['run', config.compiler, 'build'].concat(config.platforms));
-    }
-
-    // Print stdout to console.
-    cp.stdout.on('data', (data: string | Buffer) => {
-      if(typeof data === "string") 
-        console.log(`${greenBG}${data}`);
-    else 
-        console.log(`${greenBG}${data.toString()}`);
-    });
-    cp.stderr.on('data', (data: string | Buffer) => {
-      if(typeof data === "string") 
-        console.log(`${redBG}${data}`);
-      else 
-        console.log(`${redBG}${data.toString()}`);
-    });
-    cp.on('exit', (code: number) => {
-      console.log(`${greenBG}Process complete and exited with code: ${code}${resetBG}`);
-    });
-  }
-});
+  });
+})();
 
 export default liveify;
